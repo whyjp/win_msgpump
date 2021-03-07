ffa68029d79c0bc84b4273c8a19440fcb9049f48
youngjoo.park <whyj.park@gmail.com>
2021-03-07 14:55:37 +0900

    init , rand ellipse 메세지 독점중

---
+++ mfc_msgpump/mfc_msgpump/mfc_msgpump.cpp
@@ -0,0 +1,48 @@
+﻿
+// mfc_msgpump.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "mfc_msgpump.h"
+#include "mfc_msgpumpDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CmfcmsgpumpApp
+
+BEGIN_MESSAGE_MAP(CmfcmsgpumpApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CmfcmsgpumpApp 생성
+
+CmfcmsgpumpApp::CmfcmsgpumpApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CmfcmsgpumpApp 개체입니다.
+
+CmfcmsgpumpApp theApp;
+
+
+// CmfcmsgpumpApp 초기화
+
+BOOL CmfcmsgpumpApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	CmfcmsgpumpDlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+
+	return FALSE;
+}
+
---
+++ mfc_msgpump/mfc_msgpump/mfc_msgpump.h
@@ -0,0 +1,32 @@
+﻿
+// mfc_msgpump.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CmfcmsgpumpApp:
+// 이 클래스의 구현에 대해서는 mfc_msgpump.cpp을(를) 참조하세요.
+//
+
+class CmfcmsgpumpApp : public CWinApp
+{
+public:
+	CmfcmsgpumpApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CmfcmsgpumpApp theApp;
